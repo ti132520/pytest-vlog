6ea6478abe575b70db05e700b023aa80c429234b
ti132520 <ti132520@gmail.com>
2021-04-08 22:41:29 +0800

    我的测试学习记录 完成

---
+++ pytest-20210408/Calculator.py
@@ -0,0 +1,10 @@
+class Calculator:
+
+    # 简易计算器 加法
+    @staticmethod
+    def add_fun(a, b):
+        return a + b
+
+    @staticmethod
+    def div_fun(a, b):
+        return a / b
---
+++ pytest-20210408/test_calculator.py
@@ -0,0 +1,43 @@
+import pytest
+import yaml
+
+from Calculator import Calculator
+
+
+class TestCal:
+    @staticmethod
+    def setup_class():
+        print('开始计算')
+
+    @staticmethod
+    def teardown_class():
+        print('计算结束')
+
+    def setup(self):
+        self.cal = Calculator()
+
+    # pytest
+    @pytest.mark.test_add
+    @pytest.mark.parametrize('a,b', [
+        [1, 2], [2, 3]
+    ], ids=['one', 'two'])
+    # 测试用例：计算器加法测试
+    def test_add(self, a, b):
+        assert self.cal.add_fun(a, b) in [3, 5]
+
+    @pytest.mark.parametrize('a,b', [
+        [1, 1], [3, 3]
+    ])
+    # 测试用例：计算器除法测试
+    def test_div(self, a, b):
+        assert self.cal.div_fun(a, b) == 1
+
+    # yaml 参数导入1
+    @pytest.mark.parametrize('par', yaml.load(open('./par.yaml', 'r'), Loader=yaml.FullLoader))
+    def test_yaml_add(self, par):
+        assert self.cal.add_fun(par[0], par[1]) in [3, 5, 7]
+
+    # yaml 参数导入2
+    @pytest.mark.parametrize('a,b', yaml.load(open('./par.yaml', 'r'), Loader=yaml.FullLoader))
+    def test_yaml_add2(self, a, b):
+        assert self.cal.add_fun(a, b) in [3, 5, 7]
