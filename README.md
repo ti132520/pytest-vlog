61b68fe93c70f0d7fcd957a4a87cef88f74a2c7c
ti132520 <ti132520@gmail.com>
2021-04-16 02:12:32 +0800

    第三次

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 
 ### 第二次地址：[github点击这里](https://github.com/ti132520/pytest-vlog/tree/main/pytest-20210408) [gitlab点击这里](https://gitlab.stuq.ceshiren.com/1665027031/pytest-vlog/-/tree/main/pytest-20210411) [gitee点击这里](https://gitee.com/ti132520/pytest-vlog/tree/main/pytest-20210411)
 
+### 第三次地址：[github点击这里](https://github.com/ti132520/pytest-vlog/tree/main/pytest-20210415) [gitlab点击这里](https://gitlab.stuq.ceshiren.com/1665027031/pytest-vlog/-/tree/main/pytest-20210415) [gitee点击这里](https://gitee.com/ti132520/pytest-vlog/tree/main/pytest-20210415)
+
 
 
 #### *项目所有pip 依赖包都在 pip_install_list.txt 中，使用命令 pip install -r requirements.txt 即可快速安装
--- workMp-20210415/test_login_work_mp.py
+++ workMp-20210415/test_login_work_mp.py
@@ -3,21 +3,21 @@
 # File      ： test_wmp_login
 # IDE       ： PyCharm
 import time
+from random import random, randrange
 from time import sleep
 
 import pytest
 import yaml
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestWmpLogin:
-    def setup_method(self, method):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option('debuggerAddress', "127.0.0.1:9222")
-        self.driver = webdriver.Chrome(chrome_options=options)
+    # def setup_method(self, method):
+    #
 
     def teardown_method(self, method):
         self.driver.quit()
@@ -29,20 +29,40 @@ class TestWmpLogin:
     #         yaml.dump(cookies, f)
 
     def test_login_work_mp(self):
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('debuggerAddress', "127.0.0.1:9222")
+        self.driver = webdriver.Chrome(chrome_options=options)
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")
         # 导入 cookies 到网页 完成登陆
+        self.driver.delete_all_cookies()
         cookies = yaml.safe_load(open('user_datas/cookies.yaml', 'r'))
         for cookie in cookies:
             self.driver.add_cookie(cookie)
         self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")
-        # 隐性等待
-        self.driver.implicitly_wait(5)
-        # 等待 添加成员 出现再执行
-        WebDriverWait(self.driver, 10, 0.2).until(EC.presence_of_element_located(
-            By.LINK_TEXT, "添加成员124"
-        ))
-        # self.driver.find_element(By.LINK_TEXT, "添加成员").click()
-        # username = hash(time.time())
-        # num = '1888888' + range(0000, 9999)
-        # self.driver.find_element(By.ID, "username").send_keys(username)
-        # self.driver.find_element(By.ID, "memberAdd_phone").send_keys(num)
+
+        sleep(1)
+        # try:
+        #     # 等待 添加成员 出现再执行
+        #     WebDriverWait(self.driver, 1, 0.2).until(EC.presence_of_element_located((By.CLASS_NAME, 'js_add_member')))
+        # except Exception as e:
+        #     raise e
+
+        # el = self.driver.find_element(By.LINK_TEXT, '添加成员')
+        # self.driver.execute_script('arguments[0].click()', el)
+        js = 'document.getElementsByClassName("qui_btn ww_btn js_add_member")[0].click()'
+        self.driver.execute_script(js)
+        sleep(1)
+        # try:
+        #     WebDriverWait(self.driver, 1, 0.2).until(EC.presence_of_element_located((By.LINK_TEXT, '保存')))
+        #
+        # except Exception as e:
+        #     raise e
+        username = 'hash'
+        member = 'hash1'
+        num = '18888888888'
+        # + randrange(1, 9999, 4)
+        self.driver.find_element(By.ID, "username").send_keys(username)
+        self.driver.find_element(By.ID, "memberAdd_acctid").send_keys(member)
+        self.driver.find_element(By.ID, "memberAdd_phone").send_keys(num)
+        js = 'document.getElementsByClassName("qui_btn ww_btn js_btn_save")[0].click()'
+        self.driver.execute_script(js)
