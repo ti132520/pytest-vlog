9a36b50841ac3379a8eb22a312391aee3523f7fd
ti132520 <ti132520@gmail.com>
2021-04-16 02:16:41 +0800

    第三次

---
+++ unittest-20210413/test_unittest.py
@@ -0,0 +1,10 @@
+# Author    ： 怕你呀
+# Time      ： 2021/4/13
+# File      ： test_unittest
+# IDE       ： PyCharm
+import unittest
+
+
+class TestDemo(unittest.TestCase):
+    def test_1(self):
+        print(1)
