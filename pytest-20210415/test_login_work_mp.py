2dff9bba110c6518c17223954f42fd0d5fbb1722
ti132520 <ti132520@gmail.com>
2021-04-17 16:49:18 +0800

    修改

--- pytest-20210415/test_login_work_mp.py
+++ pytest-20210415/test_login_work_mp.py
@@ -5,58 +5,65 @@
 import time
 from random import random, randrange
 from time import sleep
-
-import pytest
 import yaml
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestWmpLogin:
-    # def setup_method(self, method):
-    #
-
-    def teardown_method(self, method):
-        self.driver.quit()
-    # 导出cookies 到yaml文件
-    # def test_open_with_get_cookies(self, init_driver):
-    #     self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
-    #     cookies = self.driver.get_cookies()
-    #     with open("user_datas/cookies.yaml", 'w') as f:
-    #         yaml.dump(cookies, f)
-
-    def test_login_work_mp(self):
+    def setup_method(self, method):
+        # 连接debugger chrome 浏览器
         options = webdriver.ChromeOptions()
         options.add_experimental_option('debuggerAddress', "127.0.0.1:9222")
         self.driver = webdriver.Chrome(chrome_options=options)
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")
-        # 导入 cookies 到网页 完成登陆
-        self.driver.delete_all_cookies()
-        cookies = yaml.safe_load(open('user_datas/cookies.yaml', 'r'))
-        for cookie in cookies:
-            self.driver.add_cookie(cookie)
-        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")
+        self.driver.implicitly_wait(3)
+        # self.driver.maximize_window()
 
-        sleep(1)
-        # try:
-        #     # 等待 添加成员 出现再执行
-        #     WebDriverWait(self.driver, 1, 0.2).until(EC.presence_of_element_located((By.CLASS_NAME, 'js_add_member')))
-        # except Exception as e:
-        #     raise e
+    def teardown_method(self, method):
+        self.driver.quit()
 
-        # el = self.driver.find_element(By.LINK_TEXT, '添加成员')
-        # self.driver.execute_script('arguments[0].click()', el)
-        js = 'document.getElementsByClassName("qui_btn ww_btn js_add_member")[0].click()'
-        self.driver.execute_script(js)
-        sleep(1)
-        # try:
-        #     WebDriverWait(self.driver, 1, 0.2).until(EC.presence_of_element_located((By.LINK_TEXT, '保存')))
-        #
-        # except Exception as e:
-        #     raise e
+    def test_login_work_mp(self):
+        # 打开企业微信首页
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame#index")
+
+        if self.driver.current_url == "https://work.weixin.qq.com/wework_admin/frame#index":
+            cookies = self.driver.get_cookies()
+            r_steam = open('user_datas/cookies.yaml', 'r')
+            with open("user_datas/cookies.yaml", 'w') as f:
+                if yaml.safe_load(r_steam) != cookies:
+                    yaml.dump(cookies, f)
+                f.close()
+        else:
+            # 导入 cookies 到网页 完成登陆
+            self.driver.delete_all_cookies()
+            r_steam = open('user_datas/cookies.yaml', 'r')
+            cookies = yaml.safe_load(r_steam)
+            r_steam.close()
+            for cookie in cookies:
+                self.driver.add_cookie(cookie)
+            self.driver.get("https://work.weixin.qq.com/wework_admin/frame#contacts")
+            if self.driver.current_url != "https://work.weixin.qq.com/wework_admin/frame#index":
+                print("登陆信息过期，请登陆后再试")
+            assert False
+        # 点击通讯录
+        self.driver.find_element(By.XPATH, "//a[@id='menu_contacts']").click()
+        try:
+            # 等待 添加成员 出现再执行
+            WebDriverWait(self.driver, 2, 0.2).until(EC.presence_of_element_located((By.XPATH, '//a[@class="qui_btn ww_btn js_add_member"]')))
+            # 睡眠2秒等待页面 js 功能 加载完成
+            sleep(2)
+            # 点击 添加成员 js版本
+            js_add_member = 'document.getElementsByClassName("qui_btn ww_btn js_add_member")[0].click()'
+            self.driver.execute_script(js_add_member)
+        except Exception as e:
+            raise e
+        try:
+            WebDriverWait(self.driver, 2, 0.2).until(EC.presence_of_element_located((By.XPATH, '//a[@class="qui_btn ww_btn js_btn_save"]')))
+        except Exception as e:
+            raise e
         username = 'hash'
         member = 'hash1'
         num = '18888888888'
@@ -64,5 +71,5 @@ class TestWmpLogin:
         self.driver.find_element(By.ID, "username").send_keys(username)
         self.driver.find_element(By.ID, "memberAdd_acctid").send_keys(member)
         self.driver.find_element(By.ID, "memberAdd_phone").send_keys(num)
-        js = 'document.getElementsByClassName("qui_btn ww_btn js_btn_save")[0].click()'
-        self.driver.execute_script(js)
+        js_save = 'document.getElementsByClassName("qui_btn ww_btn js_btn_save")[0].click()'
+        self.driver.execute_script(js_save)
